1edd6884568ef8a40a36ddba3f0b2725d5ecffb2
Ferphy <fernangl08@gmail.com>
2025-01-20 21:06:51 +0100

    Refactored HomeScreen code for improved readability and maintainability and added a GenericTopAppBar for details and new task screens

--- app/src/main/java/com/example/diaryapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/diaryapp/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.diaryapp.screens.home
 import android.util.Log
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -18,6 +17,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -34,18 +34,18 @@ import com.example.diaryapp.widgets.TitleOfTheSection
 import com.example.diaryapp.widgets.ToDoCard
 import com.example.diaryapp.widgets.WeekDaysHomeCard
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel,
     navController: NavController
 ) {
     val weekDays = getWeekDays()
-    var selectedDayIndex by remember { mutableIntStateOf(-1) } // Controlar el día seleccionado
+    val selectedDayIndex by remember { mutableIntStateOf(-1) }
 
     // Listas mutables para "To Do" y "In Progress"
     val toDoTasks = remember { mutableStateListOf(*getDummyData().toTypedArray()) }
     val inProgressTasks = remember { mutableStateListOf<ToDoItem>() }
+
     //ESTOS SON LOS VERDADEROS ELEMENTOS QUE TENGO QUE MOSTAR
     val taskList = homeViewModel.taskList.collectAsState().value
 
@@ -53,27 +53,7 @@ fun HomeScreen(
         Log.d("HomeScreen", "Arrastrando: $data")
     }
 
-    val onDrop: (String) -> Unit = { data ->
-        val properties = data.split(";").associate {
-            val (key, value) = it.split("=")
-            key to value
-        }
-
-        val taskTitle = properties["title"]
-        val task = toDoTasks.find { it.title == taskTitle }
-        if (task != null) {
-            toDoTasks.remove(task)
-            inProgressTasks.add(task)
-
-            Log.d("HomeScreen", "Estado actual de toDoTasks: ${
-                toDoTasks.joinToString(separator = "\n") { task_items ->
-                    "Title: ${task_items.title}, Type: ${task_items.type}, Date: ${task_items.date}, Progress: ${task_items.progress}"
-                }
-            }")
-        } else {
-            Log.d("HomeScreen", "No se encontró la tarea: $taskTitle")
-        }
-    }
+    val onDrop: (String) -> Unit = handleTaskDrop(toDoTasks, inProgressTasks)
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -95,76 +75,119 @@ fun HomeScreen(
             ) {
                 HomeTopAppBar(modifier = Modifier.padding(horizontal = 10.dp))
                 TaskToDoTitle(taskNumber = toDoTasks.size, modifier = Modifier.padding(horizontal = 10.dp))
-
-                // Semana
-                LazyRow(
-                    modifier = Modifier.padding(vertical = 15.dp, horizontal = 2.dp),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    contentPadding = PaddingValues(horizontal = 16.dp)
-                ) {
-                    items(weekDays.size) { index ->
-                        val (dayName, dayNumber) = weekDays[index]
-                        WeekDaysHomeCard(
-                            dayName = dayName,
-                            dayNumber = dayNumber,
-                            isSelected = index == selectedDayIndex, // Cambia solo si es el seleccionado
-                        ) {
-                            selectedDayIndex = index // Actualiza el índice seleccionado
-                            Log.d("HomeScreen", "Día seleccionado: $dayName")
-                        }
-                    }
-                }
-
-                // Section "To Do"
-                TitleOfTheSection(
-                    title = "To do",
-                    taskNumber = toDoTasks.size,
-                    modifier = Modifier.padding(horizontal = 10.dp)
-                )
-                LazyComponentRowWithPlaceholder(
-                    toDoTasks,
-                    onDrop = onDrop
-                ) { item ->
-                    ToDoCard(
-                        item = item, // Pasa directamente el objeto ToDoItem
-                        onDragStart = onDragStart
-                    )
-                }
-
-                // Section "In Progress"
-                TitleOfTheSection(
-                    title = "In progress",
-                    taskNumber = inProgressTasks.size,
-                    modifier = Modifier.padding(horizontal = 10.dp),
-                )
-                LazyComponentRowWithPlaceholder(
-                    tasks = inProgressTasks,
-                    onDrop = onDrop
-                ) { item ->
-                    InProgressCard(
-                        item = item,
-                        onDrop = onDrop
-                    )
-                }
-
-                // Section "Completed"
-                TitleOfTheSection(
-                    title = "Completed!",
-                    taskNumber = 0, // No hay tareas completadas aún
-                    modifier = Modifier.padding(horizontal = 10.dp),
-                )
-                LazyComponentRowWithPlaceholder(
-                    emptyList<ToDoItem>(),
-                    onDrop = onDrop
-                ) { item ->
-                    ToDoCard(
-                        item = item, // Pasa directamente el objeto ToDoItem
-                        onDragStart = onDragStart
-                    )
-                }
+                DaysOfTheWeekSelector(weekDays, selectedDayIndex)
+                DraggableItemsLists(toDoTasks, onDrop, onDragStart, inProgressTasks)
             }
         }
     }
+}
+
+@Composable
+private fun DraggableItemsLists(
+    toDoTasks: SnapshotStateList<ToDoItem>,
+    onDrop: (String) -> Unit,
+    onDragStart: (String) -> Unit,
+    inProgressTasks: SnapshotStateList<ToDoItem>
+) {
+    // Section "To Do"
+    TitleOfTheSection(
+        title = "To do",
+        taskNumber = toDoTasks.size,
+        modifier = Modifier.padding(horizontal = 10.dp)
+    )
+    LazyComponentRowWithPlaceholder(
+        toDoTasks,
+        onDrop = onDrop
+    ) { item ->
+        ToDoCard(
+            item = item,
+            onDragStart = onDragStart
+        )
+    }
+    // Section "In Progress"
+    TitleOfTheSection(
+        title = "In progress",
+        taskNumber = inProgressTasks.size,
+        modifier = Modifier.padding(horizontal = 10.dp),
+    )
+    LazyComponentRowWithPlaceholder(
+        tasks = inProgressTasks,
+        onDrop = onDrop
+    ) { item ->
+        InProgressCard(
+            item = item,
+            onDrop = onDrop
+        )
+    }
+    // Section "Completed"
+    TitleOfTheSection(
+        title = "Completed!",
+        taskNumber = 0,
+        modifier = Modifier.padding(horizontal = 10.dp),
+    )
+    LazyComponentRowWithPlaceholder(
+        emptyList<ToDoItem>(),
+        onDrop = onDrop
+    ) { item ->
+        ToDoCard(
+            item = item,
+            onDragStart = onDragStart
+        )
+    }
+}
+
+@Composable
+private fun DaysOfTheWeekSelector(
+    weekDays: List<Pair<String, String>>,
+    selectedDayIndex: Int
+) {
+    var selectedDayIndex1 = selectedDayIndex
+    LazyRow(
+        modifier = Modifier.padding(vertical = 15.dp, horizontal = 2.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(weekDays.size) { index ->
+            val (dayName, dayNumber) = weekDays[index]
+            WeekDaysHomeCard(
+                dayName = dayName,
+                dayNumber = dayNumber,
+                isSelected = index == selectedDayIndex1, // Cambia solo si es el seleccionado
+            ) {
+                selectedDayIndex1 = index // Actualiza el índice seleccionado
+                Log.d("HomeScreen", "Día seleccionado: $dayName")
+            }
+        }
+    }
+}
+
+@Composable
+private fun handleTaskDrop(
+    toDoTasks: SnapshotStateList<ToDoItem>,
+    inProgressTasks: SnapshotStateList<ToDoItem>
+): (String) -> Unit {
+    val onDrop: (String) -> Unit = { data ->
+        val properties = data.split(";").associate {
+            val (key, value) = it.split("=")
+            key to value
+        }
+
+        val taskTitle = properties["title"]
+        val task = toDoTasks.find { it.title == taskTitle }
+        if (task != null) {
+            toDoTasks.remove(task)
+            inProgressTasks.add(task)
+
+            Log.d("HomeScreen", "Estado actual de toDoTasks: ${
+                toDoTasks.joinToString(separator = "\n") { task_items ->
+                    "Title: ${task_items.title}, Type: ${task_items.type}, Date: ${task_items.date}, Progress: ${task_items.progress}"
+                }
+            }")
+        } else {
+            Log.d("HomeScreen", "No se encontró la tarea: $taskTitle")
+        }
+    }
+    return onDrop
 }
 
 
